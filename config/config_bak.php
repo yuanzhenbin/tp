30d5ba49b1dc3dc28b8e7d6ee39fae08ec305bf4
yzb <1345993533@qq.com>
2020-11-26 11:13:22 +0800

    初始模板

---
+++ config/config_bak.php
@@ -0,0 +1,10 @@
+<?php
+
+define("DB_TYPE", 'mysql');
+define("DB_HOST", '127.0.0.1');
+define("DB_USER", 'root');
+define("DB_PWD", '');
+define("DB_NAME", 'test');
+define("DB_PORT", '3306');
+define("DB_CHAR", 'utf8');
+define("DB_PREFIX", '');
