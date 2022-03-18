a000625d36abf278ba85a0fa853ab2f05cdda6ea
yuanzhenbin <yuanzhenbin@zhuge.com>
2022-03-18 11:37:48 +0800

    引入ThinkTemplate类库

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '71fcc1389e375f7e70eedba54a827983a53f618d',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '71fcc1389e375f7e70eedba54a827983a53f618d',
+            'dev_requirement' => false,
+        ),
+        'topthink/think-template' => array(
+            'pretty_version' => 'v1.0.2',
+            'version' => '1.0.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../topthink/think-template',
+            'aliases' => array(),
+            'reference' => '80f0d22a48b5ae4cee443dbbc319cb99c4a5e463',
+            'dev_requirement' => false,
+        ),
+    ),
+);
